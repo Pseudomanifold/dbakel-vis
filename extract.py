e75c53378f784dbb7b71c360f9d51bbb313be266
Bastian Rieck <bastian@rieck.me>
2024-02-15 09:46:23 +0100

    Slightly improved structure of the extraction code

--- extract.py
+++ extract.py
@@ -1,14 +1,10 @@
 import fitz
 import pandas as pd
 
-doc = fitz.open("raw/2010284.pdf")
 
-descriptions = ["Tracks", "Bridges", "Switches", "Crossings", "Structures"]
-
-current_table = []
-all_tables = []
-
-
+# Auxiliary function for creating a data frame from a set of rows. This
+# is required because I do not want to repeat the creation step for the
+# last table in the file.
 def _make_data_frame(rows):
     df = pd.DataFrame.from_records(rows)
     df = df.rename(columns=df.iloc[0])
@@ -17,30 +13,41 @@ def _make_data_frame(rows):
     return df
 
 
-# Extract tables from each page
-for page_num in range(len(doc)):
-    page = doc[page_num]
-    tabs = page.find_tables()
+if __name__ == "__main__":
+    doc = fitz.open("raw/2010284.pdf")
 
-    for tab in tabs:
-        tab = tab.extract()
+    current_table = []
+    all_tables = []
 
-        # Start a new table; the first column always signifies the
-        # county of the respective object.
-        if tab[0][0] == "LAND":
-            if len(current_table) > 0:
-                print(f"Finished table on page {page_num}")
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        tabs = page.find_tables()
 
-                all_tables.append(_make_data_frame(current_table))
-                current_table = []
+        for tab in tabs:
+            tab = tab.extract()
 
-        for row in tab:
-            current_table.append(row)
+            # Start a new table; the first column always signifies the
+            # county of the respective object.
+            if tab[0][0] == "LAND":
+                if len(current_table) > 0:
+                    print(f"Finished table on page {page_num}")
 
-if len(current_table) > 0:
-    all_tables.append(_make_data_frame(current_table))
+                    all_tables.append(_make_data_frame(current_table))
+                    current_table = []
 
-assert len(all_tables) == len(descriptions)
+            for row in tab:
+                current_table.append(row)
 
-for table, description in zip(all_tables, descriptions):
-    table.to_csv(f"tables/{description}.csv", index=False)
+    # Finish the last table in the file (we can only do this _after_
+    # parsing all lines).
+    if len(current_table) > 0:
+        all_tables.append(_make_data_frame(current_table))
+
+    descriptions = ["Tracks", "Bridges", "Switches", "Crossings", "Structures"]
+
+    # Basic sanity check; if this goes wrong, you have the wrong file
+    # somehow...
+    assert len(all_tables) == len(descriptions)
+
+    for table, description in zip(all_tables, descriptions):
+        table.to_csv(f"tables/{description}.csv", index=False)
